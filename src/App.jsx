b6c19bd898187f7b13164ee06b117def69918292
Hughchen2046 <hughchen2046@gmail.com>
2026-02-12 15:06:28 +0800

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import './App.css';
-import { HashRouter, NavLink, Routes, Route } from 'react-router-dom';
+import { HashRouter, NavLink, Routes, Route, useNavigate } from 'react-router-dom';
 
 
 const Todo = () => {
-  return <p>這是 Todo 頁面</p>;
+  return <div>
+    <p>這是 Todo 頁面</p>
+    <Logout />
+  </div>;
 };
+export const Logout = () => {
+  let navigate = useNavigate();
+  return <button type='button' className='btn btn-primary' onClick={()=> {navigate('/login', { replace: true });}}>登出</button>;
+};
+
 const Login = () => {
   return <p>這是登入頁面</p>;
 };
@@ -23,17 +30,17 @@ function App() {
   return (
     <div className="container">
       <HashRouter>
-        <div className="nav-link">
-          <NavLink to="/">
+        <div className="nav nav-tabs">
+          <NavLink className="nav-link" to="/">
             <p>回到首頁</p>
           </NavLink>
-          <NavLink to="/register">
+          <NavLink className="nav-link" to="/register">
             <p>註冊頁面</p>
           </NavLink>
-          <NavLink to="/login">
+          <NavLink className="nav-link" to="/login">
             <p>登入頁面</p>
           </NavLink>
-          <NavLink to="/todo">
+          <NavLink className="nav-link" to="/todo">
             <p>Todo 頁面</p>
           </NavLink>
         </div>
